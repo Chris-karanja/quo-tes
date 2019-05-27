b09bb6a51c80b706babac7b8d59b0a83608c927b
Chris-karanja <chriswainaina0@gmail.com>
2019-05-27 18:58:53 +0300

    the days passed counter works

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
   userQuotes = [
     new Quote(1, 'Dont cry because its over, smile because it happened.', '-	Dr. Seuss', 0, 0, new Date(2019,7,10)),
     new Quote(2, 'Start where you are. Use what you have. Do what you can.', '-	Arthur Ashe', 0, 0, new Date(2019,7,10)),
-    new Quote(3, 'A room without books is like a body without a soul.', '- Marcus Tullius Cicero', 0, 0, new Date(2019,7,10)),
+    new Quote(3, 'A room without books is like a body without a soul.', '- Marcus Tullius Cicero', 0, 0, new Date(2019,1,23)),
     new Quote(4, 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.', '-	Albert Einstein', 0, 0, new Date(2018,7,10)),
     new Quote(5, 'So many books, so little time.', '- Frank Zappa', 0, 0, new Date(2019,7,10))
   ]
